cff81e4d761aebd77d8eaa166909eedd7a264b16
Srujan <ssrujan8@gmail.com>
2024-02-15 17:43:18 +0530

    consumer example block statement

--- src/com/mordernjava/functionalprogramming/ConsumerExample.java
+++ src/com/mordernjava/functionalprogramming/ConsumerExample.java
@@ -4,7 +4,16 @@ import java.util.function.Consumer;
 
 public class ConsumerExample {
     public static void main(String[] args) {
-        Consumer<String> c = (x) -> System.out.println(x + " ,the length of the string:"+x.length());
+        Consumer<String> c = (x) -> System.out.println(x + " ,length of the string is : "+x.length());
         c.accept("Up in the air");
+
+        //Consumer with block statement
+
+        Consumer<Integer> d = x -> {
+            System.out.println("x*x = " + x*x );
+            System.out.println("x/x = " + x/x);
+        };
+        d.accept(10);
+
     }
 }
