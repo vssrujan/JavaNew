1480b422ad0145a0902722d90ebb7c8f16973c35
Srujan <ssrujan8@gmail.com>
2024-02-15 13:52:51 +0530

    Traditional Implementation of Concatenate interface

---
+++ src/com/git/javanew/ConcatenateTraditional.java
@@ -0,0 +1,14 @@
+package com.git.javanew;
+
+public class ConcatenateTraditional implements ConcatenateInterface{
+
+    @Override
+    public String sconcat(String a, String b) {
+        return a + " " + b;
+    }
+
+    public static void main(String[] args) {
+        ConcatenateTraditional concatenateTraditional = new ConcatenateTraditional();
+        System.out.println(concatenateTraditional.sconcat("Hello","World"));
+    }
+}
