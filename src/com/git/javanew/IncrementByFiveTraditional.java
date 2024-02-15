adaab5db9948af9299d95ae8416c941982d8786c
Srujan <ssrujan8@gmail.com>
2024-02-15 13:41:02 +0530

    IncrementByFive Traditional class by implementing Interface.

---
+++ src/com/git/javanew/IncrementByFiveTraditional.java
@@ -0,0 +1,14 @@
+package com.git.javanew;
+
+public class IncrementByFiveTraditional implements IncrementByFiveInterface{
+
+    @Override
+    public int incrementByFive(int a) {
+        return a + 5;
+    }
+
+    public static void main(String[] args) {
+        IncrementByFiveTraditional incrementByFiveTraditional = new IncrementByFiveTraditional();
+        System.out.println(incrementByFiveTraditional.incrementByFive(2));
+    }
+}
